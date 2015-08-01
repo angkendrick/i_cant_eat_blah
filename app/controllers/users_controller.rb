c6556c08cf24d1853d9b46889a7028afb8a56d60
Ken Ang <angkendrick@gmail.com>
2015-08-01 18:29:29 -0400

    added set recipes/index as root, added links to root, added user show and edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(secure_params)
+
+    if @user.save
+      redirect_to user_path(params[:id]), notice: 'updated!'
+    else
+      render :edit
+    end
+  end
+
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
+  root to: 'recipes#index'
+
   resources :sessions, only: [:new, :create, :destroy]
 
-  resources :users, only: [:new, :create, :edit]
+  resources :users, only: [:new, :create, :edit, :update, :show]
   resources :recipes
 
   namespace :admin do
     resources :users
-    resources :ingredients, only: [:new, :create, :edit, :update, :destroy]
+    resources :ingredients, only: [:new, :create, :edit, :update, :destroy, :index]
   end
   
 end
